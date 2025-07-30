118e09561ed4d36a9dda5bd4c5a0b241f8328850
CristianAngarita <angarita99@hotmail.com>
2025-07-30 15:44:37 -0500

    Dia 1 Conexion mysql sin arq hexagonal

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"context"
+	"database/sql"
 	"log"
 	"os"
 	"time"
@@ -22,7 +23,29 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
+	// Obtener la URI de MySQL desde las variables de entorno
+	mysqlURI := os.Getenv("MYSQL_URI")
+	if mysqlURI == "" {
+		log.Fatal("MYSQL_URI environment variable not set")
+	}
+
+	// 1. Conexión a MySQL
+	db, err := sql.Open("mysql", mysqlURI)
+	if err != nil {
+		log.Fatalf("Error al abrir la conexión a MySQL: %v", err)
+	}
+	defer db.Close()
+
+	// Verificar la conexión
+	ctxDB, cancelDB := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelDB()
+	if err = db.PingContext(ctxDB); err != nil {
+		log.Fatalf("Error al hacer ping a la base de datos MySQL: %v", err)
+	}
+	log.Println("Conexión a MySQL establecida con éxito!")
+
 	ginEngine := gin.Default()
+
 	ginEngine.POST("/players", func(ctx *gin.Context) {
 		var player Player
 		if err := ctx.BindJSON(&player); err != nil {
@@ -32,27 +55,27 @@ func main() {
 
 		player.CreationTime = time.Now().UTC()
 
-		c, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		defer cancel()
-
-		client, err := mongo.Connect(c, options.Client.ApplyURI(os.Getenv("MONGO_URI")))
+		// 2. Ejecutar la consulta de inserción en MySQL
+		query := `INSERT INTO players (name, age, creation_time) VALUES (?, ?, ?)`
+		result, err := db.ExecContext(ctx.Request.Context(), query, player.Name, player.Age, player.CreationTime)
 		if err != nil {
-			log.Fatal(err)
+			log.Printf("Error al insertar jugador en MySQL: %v", err)
+			ctx.JSON(500, gin.H{"error": "No se guardó jugador"})
+			return
 		}
 
-		err = client.Ping(c, nil)
+		// Obtener el ID generado automáticamente por MySQL
+		id, err := result.LastInsertId()
 		if err != nil {
-			log.Fatal(err)
+			log.Printf("Error al obtener el ID generado: %v", err)
+			//  éxito parcial si la inserción fue exitosa
+			ctx.JSON(200, gin.H{"message": "Player created, pero no se pudo recuperar la ID"})
+			return
 		}
 
-		collection := client.Database("go-l").Collection("players")
-		insertResult, err := collection.InsertOne(c, player)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		ctx.JSON(200, gin.H{"player_id": insertResult.InsertedID})
+		ctx.JSON(200, gin.H{"player_id": id})
 	})
 
 	log.Fatalln(ginEngine.Run(":8001"))
+
 }
