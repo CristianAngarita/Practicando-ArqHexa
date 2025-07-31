691c99cc4dce1e4617dd6d5bd6be84a330a50ca4
CristianAngarita <angarita99@hotmail.com>
2025-07-31 16:32:14 -0500

    Se separa el handler player

---
+++ cmd/api/handlers/player/create.go
@@ -0,0 +1,55 @@
+package player
+
+import (
+	"database/sql"
+	"log"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Player struct {
+	Name         string    `json:"name" binding:"required"`
+	Age          int       `json:"age" binding:"required"`
+	CreationTime time.Time `json:"-"`
+}
+
+type PlayerHandler struct {
+	DB *sql.DB // The database connection
+}
+
+func NewPlayerHandler(db *sql.DB) *PlayerHandler {
+	return &PlayerHandler{DB: db}
+}
+
+func (ph *PlayerHandler) CreatePlayer(ctx *gin.Context) {
+
+	var player Player
+	if err := ctx.BindJSON(&player); err != nil {
+		ctx.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	player.CreationTime = time.Now().UTC()
+
+	// 2. Ejecutar la consulta de inserción en MySQL
+	query := `INSERT INTO players (name, age, creation_time) VALUES (?, ?, ?)`
+	result, err := ph.DB.ExecContext(ctx.Request.Context(), query, player.Name, player.Age, player.CreationTime)
+	if err != nil {
+		log.Printf("Error al insertar jugador en MySQL: %v", err)
+		ctx.JSON(500, gin.H{"error": "No se guardó jugador"})
+		return
+	}
+
+	// Obtener el ID generado automáticamente por MySQL
+	id, err := result.LastInsertId()
+	if err != nil {
+		log.Printf("Error al obtener el ID generado: %v", err)
+		//  éxito parcial si la inserción fue exitosa
+		ctx.JSON(200, gin.H{"message": "Player created, pero no se pudo recuperar la ID"})
+		return
+	}
+
+	ctx.JSON(200, gin.H{"player_id": id})
+
+}
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,17 +5,12 @@ import (
 	"database/sql"
 	"log"
 	"os"
+	"proyecto-gin-hexagonal/cmd/api/handlers/player"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
-
-type Player struct {
-	Name         string    `json:"name" binding:"required"`
-	Age          int       `json:"age" binding:"required"`
-	CreationTime time.Time `json:"-"`
-}
 
 func main() {
 	err := godotenv.Load()
@@ -44,38 +39,12 @@ func main() {
 	}
 	log.Println("Conexión a MySQL establecida con éxito!")
 
+	playerHandler := player.NewPlayerHandler(db)
 	ginEngine := gin.Default()
 
 	ginEngine.POST("/players", func(ctx *gin.Context) {
-		var player Player
-		if err := ctx.BindJSON(&player); err != nil {
-			ctx.JSON(400, gin.H{"error": err.Error()})
-			return
-		}
-
-		player.CreationTime = time.Now().UTC()
-
-		// 2. Ejecutar la consulta de inserción en MySQL
-		query := `INSERT INTO players (name, age, creation_time) VALUES (?, ?, ?)`
-		result, err := db.ExecContext(ctx.Request.Context(), query, player.Name, player.Age, player.CreationTime)
-		if err != nil {
-			log.Printf("Error al insertar jugador en MySQL: %v", err)
-			ctx.JSON(500, gin.H{"error": "No se guardó jugador"})
-			return
-		}
-
-		// Obtener el ID generado automáticamente por MySQL
-		id, err := result.LastInsertId()
-		if err != nil {
-			log.Printf("Error al obtener el ID generado: %v", err)
-			//  éxito parcial si la inserción fue exitosa
-			ctx.JSON(200, gin.H{"message": "Player created, pero no se pudo recuperar la ID"})
-			return
-		}
-
-		ctx.JSON(200, gin.H{"player_id": id})
+		playerHandler.CreatePlayer(ctx)
 	})
-
 	log.Fatalln(ginEngine.Run(":8001"))
 
 }
